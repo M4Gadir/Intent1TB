e77636d02428e40418d6878b746041c71e2b03b1
TB <habibthea2015@gmail.com>
2019-02-26 21:55:12 +0700

    altercode

--- app/src/main/java/com/magad/intent_1_tb/fragment/SecondFragment.java
+++ app/src/main/java/com/magad/intent_1_tb/fragment/SecondFragment.java
@@ -3,6 +3,7 @@ package com.magad.intent_1_tb.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -32,7 +33,7 @@ public class SecondFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_second, container, false);
         rv = v.findViewById(R.id.rv2);
-        rv.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false));
+        rv.setLayoutManager(new GridLayoutManager(getActivity(),2));
         adapter = new Adapterr(getActivity());
         rv.setAdapter(adapter);
         return v;
--- app/src/main/java/com/magad/intent_1_tb/fragment/ThirdFragment.java
+++ app/src/main/java/com/magad/intent_1_tb/fragment/ThirdFragment.java
@@ -1,11 +1,15 @@
 package com.magad.intent_1_tb.fragment;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.magad.intent_1_tb.R;
 
@@ -13,6 +17,10 @@ import com.magad.intent_1_tb.R;
  * A simple {@link Fragment} subclass.
  */
 public class ThirdFragment extends Fragment {
+
+    TextInputEditText edt,edtdua,edttiga;
+
+    Button btn,btndua,btntiga;
 
 
     public ThirdFragment() {
@@ -24,7 +32,46 @@ public class ThirdFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false);
+        View v = inflater.inflate(R.layout.fragment_third, container, false);
+        btn = v.findViewById(R.id.button1);
+        btndua = v.findViewById(R.id.button2);
+        btntiga = v.findViewById(R.id.button3);
+        edt = v.findViewById(R.id.edt1);
+        edtdua = v.findViewById(R.id.edt2);
+        edttiga = v.findViewById(R.id.edt3);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", String.valueOf(edt.getText()),null)));
+            }
+        });
+        btndua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // jadi tadi saya ngikutin langsung memakai button dan di masukin ke start activity
+                startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + edtdua.getText())));
+            }
+        });
+        btntiga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (getView().getId()){
+                    case R.id.button3:
+                        if (edttiga.getText().toString().trim().length() == 0) {
+                        edttiga.setError("antum lupa mengisi url disini");
+                        }
+                        break;
+                }
+
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www" + String.valueOf(edttiga.getText()))));
+            }
+        });
+
+        return v;
     }
+
+
 
 }
